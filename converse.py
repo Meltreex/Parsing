f7c706dc32f306243acd7d73dd4f1110f5cc4521
Metreex <144696893+Meltreex@users.noreply.github.com>
2024-02-07 01:08:20 +1000

    Update converse.py
    edit_code#3

--- converse.py
+++ converse.py
@@ -1,18 +1,16 @@
 import json
-
-
 import pandas as pd
-
+#Создаем датасет с названиями столбцов
 df = pd.DataFrame(columns=['Название', 'Город', 'Описание', 'Ссылки'])
-
+#Открываем фалй на чтение
 with open('brands_list.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-
+#Переносим все данные в датасет с помощью метода loc
 for key, value in data.items():
 
     df.loc[len(df.index)] = value
-
+#Преобразуем в csv и xlsx
 df.to_csv('output.csv')
 df.to_excel('output.xlsx')
 
